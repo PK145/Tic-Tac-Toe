2359e23c86fd52d410fe9abd0739c7e186200834
PK145 <119965577+PK145@users.noreply.github.com>
2022-12-21 12:32:48 +0000

    Vertical Winning Condition

--- main.py
+++ main.py
@@ -28,4 +28,10 @@ def CheckWin():
     elif(board[4] == board[5] and board[5] == board[6] and board[4] != ' '):    
         Game = Win    
     elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):    
-        Game = Win         
+        Game = Win    
+    elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):    
+        Game = Win    
+    elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):    
+        Game = Win    
+    elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):    
+        Game=Win         
