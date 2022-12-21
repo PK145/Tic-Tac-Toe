3231534c6566ab7c89fd6135e5fb675033abe4c2
PK145 <119965577+PK145@users.noreply.github.com>
2022-12-21 12:27:25 +0000

    This Function Draws Game Board

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ Win = 1
 Draw = -1    
 Running = 0    
 Stop = 1    
+Game = Running    
+Mark = 'X'    
+def DrawBoard():    
+    print(" %c | %c | %c " % (board[1],board[2],board[3]))    
+    print("___|___|___")    
+    print(" %c | %c | %c " % (board[4],board[5],board[6]))    
+    print("___|___|___")    
+    print(" %c | %c | %c " % (board[7],board[8],board[9]))    
+    print("   |   |   ")    
